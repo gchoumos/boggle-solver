b980dc023d37f94240b463fc0c2d839a3fb808fc
choumosg <g.choumos@gmail.com>
2020-01-02 18:57:00 +0200

    Remove unused function and unecessary stuff. Enhance dictionary (now around 7k words)

--- boggle.py
+++ boggle.py
@@ -6,13 +6,8 @@ with open('gr_dict') as f:
 
 dictionary = [x.strip() for x in dictionary]
 
-def read_boggle(boggle, n=4):
-    if len(boggle) != n*n:
-        print("No {0}x{0} boggle can be created".format(n))
-
 # Represents the full boggle puzzle
 # boggle_string is the letters as a single string (left to right, top to bottom)
-# n is the size of the boggle (n x n)
 class Boggle:
     def __init__(self,boggle_string,n):
         print("Hello from Boggle constructor")
@@ -33,22 +28,16 @@ class Boggle:
         print("Boggle array and neighbors are ready.")
     #
     def word_search(self,i,j,visited,word):
-        # print('In ({0},{1}) - Current word: {2}'.format(i,j,word))
         to_visit = [x for x in self.neighbors[i,j] if x not in visited]
-        # print("For ({0},{1}) I'll visit {2}".format(i,j,to_visit))
         if word != '' and str(word+self.boggle_arr[i][j]) in dictionary:
             self.boggle_words.append(str(word+self.boggle_arr[i][j]))
         word = str(word+self.boggle_arr[i][j])
         visited.append((i,j))
-        # Check if we need to stop - (this is not optimal - still checking unecessary things)
+        # Check if we need to stop (no words in dictionary exist that start with current word)
         if any((w.startswith(word) and (len(word) < len(w))) for w in dictionary):
-            # the prefix we have may lead to a word, let's continue
-            # print('words starting with {0} exist. Will check:{1}'.format(word,to_visit))
             for neighbor in to_visit:
                 self.word_search(neighbor[0],neighbor[1],visited,word)
                 visited.remove((neighbor[0],neighbor[1]))
-        # else:
-        #     print("No words starting with {0} - Moving on".format(word))
     #
     def solve(self):
         for i in range(self.n):
